6eaa44d3c898e7549dbf7bb511e1459e93f856e7
Erik Owen <erikjowen@yahoo.com>
2013-10-28 15:50:49 -0700

    first commit

---
+++ src/bev/and/owe/HomeScreen.java
@@ -0,0 +1,27 @@
+package bev.and.owe;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HomeScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home_screen);
+		initLayout();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+	
+	protected void initLayout() {
+		
+	}
+
+}
