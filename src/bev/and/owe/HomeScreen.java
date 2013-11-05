50aa9209b3f6c280efc224617aed1a8919e09bcd
Brandon Bevans <brandonbevans@gmail.com>
2013-11-04 21:51:07 -0800

    upgraded to have sign in stuff

--- src/bev/and/owe/HomeScreen.java
+++ src/bev/and/owe/HomeScreen.java
@@ -3,6 +3,7 @@ package bev.and.owe;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
@@ -12,24 +13,37 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.TextView;
 import android.widget.Toast;
+import bev.and.owe.*;
 
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.games.GamesClient;
+import com.google.android.gms.plus.PlusClient;
+import com.google.android.gms.appstate.*;
+import com.google.example.games.basegameutils.BaseGameActivity;
 
-public class HomeScreen extends Activity {
+public class HomeScreen extends BaseGameActivity {
 
 	protected Button timedPlay_button;
 	protected Button freePlay_button;
 	protected Button highScores_button;
 	protected Button settings_button;
 	
-	@Override
+	protected GamesClient gameClient;
+	protected Button pointButton;
+	protected TextView pointValue;
+	
+	protected Button signInButton = (Button) findViewById(R.id.signInButton);
+	protected Button signOutButton = (Button) findViewById(R.id.signOutButton);
+
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_screen);
 		
 		initLayout();
-		initAddJokeListeners();
+		initAddLeaderboard();
 	}
 
 	@Override
@@ -41,23 +55,76 @@ public class HomeScreen extends Activity {
 	
 	protected void initLayout() {
 		setContentView(R.layout.home_screen);
+		
+		pointButton = (Button) findViewById(R.id.pointButton);
+		pointValue = (TextView) findViewById(R.id.pointValue);
+		
 	}
 	
-	protected void initAddJokeListeners() {
+	protected void initAddLeaderboard() {
 		int connectionResult = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
 		switch (connectionResult) {
 		case 0:
 			Toast.makeText(this, "Google Play Services: Success", Toast.LENGTH_LONG).show();
 			break;
 		default:
-			Toast.makeText(this, "Google Play Services: Error Occured", Toast.LENGTH_LONG).show();
+			Toast.makeText(this, "Error code: " + connectionResult, Toast.LENGTH_LONG).show();
 			break;
 		}
-		/*this.highScores_button.setOnClickListener(new OnClickListener() {
+		this.pointButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View view) {
-				
+				String pointString = pointValue.getText().toString();
+				try {
+					Integer points = Integer.parseInt(pointString);
+					Log.d("POINTS", points.toString());
+					
+				} catch (Exception e) {
+					
+				}
 			}
-		});*/
+		});
+	}
+		/*
+		this.signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // start the asynchronous sign in flow
+                beginUserInitiatedSignIn();
+            }
+        });
+		
+		this.signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+                setSigninButtonState();
+            }
+        });
+        
+	}
+	
+	
+	public void setSigninButtonState() {        
+        if(isSignedIn()) {
+            findViewById(R.id.signOutButton).setVisibility(View.VISIBLE);
+            findViewById(R.id.signInButton).setVisibility(View.GONE);
+        } else {
+            findViewById(R.id.signOutButton).setVisibility(View.GONE);
+            findViewById(R.id.signInButton).setVisibility(View.VISIBLE);          
+        }           
+    }
+
+	*/
+	@Override
+	public void onSignInFailed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSignInSucceeded() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
