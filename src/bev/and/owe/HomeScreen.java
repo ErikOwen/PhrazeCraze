b73f705fe626d22d49770028542ef28dfc403135
Erik Owen <erikjowen@yahoo.com>
2013-11-04 19:59:43 -0800

    added google play services test code

--- src/bev/and/owe/HomeScreen.java
+++ src/bev/and/owe/HomeScreen.java
@@ -2,15 +2,34 @@ package bev.and.owe;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 public class HomeScreen extends Activity {
 
+	protected Button timedPlay_button;
+	protected Button freePlay_button;
+	protected Button highScores_button;
+	protected Button settings_button;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_screen);
+		
 		initLayout();
+		initAddJokeListeners();
 	}
 
 	@Override
@@ -22,6 +41,23 @@ public class HomeScreen extends Activity {
 	
 	protected void initLayout() {
 		setContentView(R.layout.home_screen);
+	}
+	
+	protected void initAddJokeListeners() {
+		int connectionResult = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		switch (connectionResult) {
+		case 0:
+			Toast.makeText(this, "Google Play Services: Success", Toast.LENGTH_LONG).show();
+			break;
+		default:
+			Toast.makeText(this, "Google Play Services: Error Occured", Toast.LENGTH_LONG).show();
+			break;
+		}
+		/*this.highScores_button.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
+				
+			}
+		});*/
 	}
 
 }
