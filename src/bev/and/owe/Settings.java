6fe4330c07dcddef98508040ae50518465eeef74
Brandon Bevans <brandonbevans@gmail.com>
2013-11-17 22:20:13 -0800

    Added Settings template

---
+++ src/bev/and/owe/Settings.java
@@ -0,0 +1,26 @@
+package bev.and.owe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Settings extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		initLayout();
+		initOnClickListeners();
+	}
+
+	private void initOnClickListeners() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initLayout() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
